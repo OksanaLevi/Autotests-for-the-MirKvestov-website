09a41a40a7a1b6594a4814e984d68f2c405b324d
Oksana Levinskaya <qa.oksanaa@gmail.com>
2023-11-28 19:26:55 +0300

    add selenoid UI

--- src/test/java/ru/mirkvwstov/tests/TestBase.java
+++ src/test/java/ru/mirkvwstov/tests/TestBase.java
@@ -30,7 +30,7 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
 //        Configuration.holdBrowserOpen = true;
 
-//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
